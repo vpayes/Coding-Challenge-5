2b456fc1f141fde68bc542f30415620e8520a1b4
vpayes <vanesa_payes@outlook.com>
2024-09-22 20:51:20 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,7 @@
+// Task 1: Create an Inventory Array of Product Objects 
+const inventory = [
+    {name: 'Espresso', price: 3, quantity: 10 },
+    { name: 'Latte', price: 4, quantity: 5 },
+    { name: 'Cappuccino', price: 4, quantity: 6 },
+    { name: 'Mocha', price: 5, quantity: 4 }
+];
